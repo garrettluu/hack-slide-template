8024f0a30cd5cd881c1f6fdce33972a319f09fc1
Garrett Luu <garrettluu131@gmail.com>
2020-11-23 14:16:02 -0800

    Small updates

--- theme.js
+++ theme.js
@@ -1,6 +1,8 @@
+import { prism } from '@mdx-deck/themes';
 const orange = "#F9A857";
 const black = "#333333";
 const white = "#FFFFFF";
+const darkOrange = "#271400";
 
 export default {
   googleFont: 'https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap',
@@ -17,7 +19,13 @@ export default {
     body: 400,
     heading: 700,
   },
+  components: {
+    ...prism.components
+  },
   styles: {
+    b: {
+      color: 'primary',
+    },
     h1: {
       fontSize: 84,
       fontWeight: 'heading',
