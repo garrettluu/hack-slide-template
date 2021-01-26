2792ad4537aeace5b4a97ed52eb260bcef9cb25d
Garrett Luu <garrettluu131@gmail.com>
2021-01-25 21:57:28 -0800

    Added code surfer and examples

--- theme.js
+++ theme.js
@@ -1,8 +1,26 @@
-import { prism } from '@mdx-deck/themes';
+import React from 'react';
+import { Prism } from 'react-syntax-highlighter'
+import * as prismThemes from 'react-syntax-highlighter/dist/esm/styles/prism';
 import './theme.css';
 const orange = "#F9A857";
 const black = "#333333";
 const white = "#FFFFFF";
+
+const getLanguage = className => {
+  const match = /language-(\w*)/.exec(className || 'language-javascript')
+  let lang = 'javascript'
+  if (match && match.length > 1) {
+    lang = match[1]
+  }
+  return lang
+}
+
+export const pre = props => props.children
+
+export const code = props => {
+  const language = getLanguage(props.className)
+  return <Prism language={language} style={prismThemes.cb} {...props} />
+}
 
 export default {
   googleFont: 'https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap',
@@ -20,7 +38,8 @@ export default {
     heading: 700,
   },
   components: {
-    ...prism.components
+    pre,
+    code,
   },
   styles: {
     b: {
